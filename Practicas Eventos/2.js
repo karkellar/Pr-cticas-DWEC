7d1278413c71a5bc7e8621be6f3641e558a55e51
Karla <karjuacas@gmail.com>
2025-10-28 19:20:44 +0100

    eventos2

--- Practicas Eventos/2.js
+++ Practicas Eventos/2.js
@@ -1,10 +1,30 @@
-function validateOption() {
-    let email = document.getElementById ("email");
-    let password = document.getElementById ("password");
-    if (!email.value.includes("@")) {
-        emailError.textContent = ("Email no válido");
-    }
-    if (password.value.length < 8 || password.value.length > 10) {
-        passwordError.textContent = ("Contraseña no válida")
-    }
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let emailError = document.getElementById("emailError");
+let passwordError = document.getElementById("passwordError");
+let button = document.getElementById("send");
+let emailValido = true;
+let passwordValida = true;
+function activeButton() {
+    button.disabled = !(emailValido && passwordValida);
 }
+email.addEventListener("blur", function errorEmail() {
+    if (!email.value.includes('@')) {
+        emailError.textContent = ("Email no válido, debe contener un @");
+        emailValido = false;
+    }
+    activeButton();
+})
+password.addEventListener("blur", function errorPassword() {
+    if (password.value.length < 8 || password.value.length > 10) {
+        passwordError.textContent = ("Contraseña no válida, debe tener una longitud entre 8 y 10 carácteres");
+        passwordValida = false;
+    }
+    activeButton();   
+})
+
+document.getElementById("form").addEventListener("submit", function(e) {
+    if (!(emailValido && passwordValida)) {
+        alert("Por favor, corrige los fallos y vuelve a intentarlo :)");
+    }
+});
